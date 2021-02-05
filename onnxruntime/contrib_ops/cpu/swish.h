d56b4dcf0874bdbdd666da6857d4edac889018ef
HemaSowjanyaMamidi <47590467+HemaSowjanyaMamidi@users.noreply.github.com>
2021-02-05 13:28:38 +0530

    Update swish.h

--- onnxruntime/contrib_ops/cpu/swish.h
+++ onnxruntime/contrib_ops/cpu/swish.h
@@ -1,6 +1,10 @@
+#include "core/common/common.h"
+#include "core/framework/op_kernel.h"
+
 namespace onnxruntime {
 namespace contrib {
- class Swish final : public OpKernel {
+template <typename T>
+ class Swish : public OpKernel {
  public:
   explicit Swish(const OpKernelInfo& info) : OpKernel(info) {}
   
