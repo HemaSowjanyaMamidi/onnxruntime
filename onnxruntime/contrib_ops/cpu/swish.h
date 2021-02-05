2279ab7edd389551129aad62e84fbc9b2219775f
HemaSowjanyaMamidi <47590467+HemaSowjanyaMamidi@users.noreply.github.com>
2021-02-05 12:16:03 +0530

    Update swish.h

--- onnxruntime/contrib_ops/cpu/swish.h
+++ onnxruntime/contrib_ops/cpu/swish.h
@@ -1,37 +1,10 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-#pragma once
-
-#include "core/common/common.h"
-#include "core/framework/op_kernel.h"
-#include "core/framework/tensor.h"
-
-#include <cmath>
-
-using namespace std; 
-
 namespace onnxruntime {
 namespace contrib {
-template <typename T>
-class Swish : public OpKernel {
+ class Swish final : public OpKernel {
  public:
   explicit Swish(const OpKernelInfo& info) : OpKernel(info) {}
-
-  Status Compute(OpKernelContext* context) const override {
-    auto X = context->Input<Tensor>(0);
-    auto& dims = X->Shape();
-    auto Y = context->Output(0, dims);
-    
-    auto X_Data = (X->template Data<T>());
-    auto Y_Data = (Y->template MutableData<T>());
-
-    for (int64_t i = 0, sz = dims.Size(); i < sz; i++,Y_Data++,X_Data++) {
-      *Y_Data = *X_Data / (exp(-*X_Data) + 1);
-    }
-
-    return Status::OK();
-  }
+  
+  Status Compute(OpKernelContext* ctx) const override;
 };
 }  // namespace contrib
 }  // namespace onnxruntime
