840e729b564581e6b27b0a4b67803441d0df3a51
HemaSowjanyaMamidi <47590467+HemaSowjanyaMamidi@users.noreply.github.com>
2021-02-05 13:28:05 +0530

    Update swish.cc

--- onnxruntime/contrib_ops/cpu/swish.cc
+++ onnxruntime/contrib_ops/cpu/swish.cc
@@ -8,21 +8,21 @@
 namespace onnxruntime {
 namespace contrib {
 
-ONNX_OPERATOR_KERNEL_EX(
-    Swish,
-    kOnnxDomain,
-    1,
-    kCpuExecutionProvider,
-    KernelDefBuilder().TypeConstraint("T", DataTypeImpl::GetTensorType<float,double>()),
-    Swish);
+#define DEFINE_KERNEL(data_type)                                                                                  \
+  ONNX_OPERATOR_TYPED_KERNEL_EX(Swish, kOnnxDomain, 1, data_type, kCpuExecutionProvider,                            \
+                                KernelDefBuilder().TypeConstraint("T", DataTypeImpl::GetTensorType<data_type>()), \
+                                Swish<data_type>);
+DEFINE_KERNEL(float);
+DEFINE_KERNEL(double);
+
     
 template <typename T>
-static Status SwisImpl(const Tensor* X, Tensor* Y) {
+static Status SwishImpl(const Tensor* X, Tensor* Y) {
   const auto& X_shape = X->Shape();
   int64_t X_num_dims = static_cast<int64_t>(X_shape.NumDimensions());
   const auto* X_data = reinterpret_cast<const T*>(X->DataRaw());
   auto* Y_data = reinterpret_cast<T*>(Y->MutableDataRaw());
-  for (int64_t i = 0, sz = dims.Size(); i < sz; i++,Y_Data++,X_Data++) {
+  for (int64_t i = 0, sz = X_shape.Size(); i < sz; i++,Y_Data++,X_Data++) {
       *Y_Data = *X_Data / (exp(-*X_Data) + 1);
     }
 
